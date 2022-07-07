a97c1ba82b57e3906f565e4fcc11f86ed6c725a8
Musti666 <101584248+Musti666@users.noreply.github.com>
2022-07-07 23:01:05 +0200

    Add files via upload

---
+++ bin/Debug/net6.0/89952af4f10c40d6b56c64db55dee003.sh
@@ -0,0 +1,30 @@
+function list_child_processes(){
+    local ppid=$1;
+    local current_children=$(pgrep -P $ppid);
+    local local_child;
+    if [ $? -eq 0 ];
+    then
+        for current_child in $current_children
+        do
+          local_child=$current_child;
+          list_child_processes $local_child;
+          echo $local_child;
+        done;
+    else
+      return 0;
+    fi;
+}
+
+ps 60163;
+while [ $? -eq 0 ];
+do
+  sleep 1;
+  ps 60163 > /dev/null;
+done;
+
+for child in $(list_child_processes 60278);
+do
+  echo killing $child;
+  kill -s KILL $child;
+done;
+rm /Users/maximiliankriegl/Desktop/FH Stuff/ITP/Companion/bin/Debug/net6.0/89952af4f10c40d6b56c64db55dee003.sh;
